b2532861fc39ae5b3475ff1a2119ce8fa63b8b3c
Matheus Pimentel <matheus08.pimentel@gmail.com>
2020-08-08 11:56:46 -0300

    Finish the mock

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import hearOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://github.com/matheuscpimentel.png' }}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Matheus Pimentel</Text>
+          <Text style={styles.subject}>Programação</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Teste testando teste teste teste eeeeeeeeee
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'   '}
+          <Text style={styles.priceValue}>R$20,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={[styles.favoriteButton, styles.favorited]}>
+            {/* <Image source={hearOutlineIcon} /> */}
+            <Image source={unfavoriteIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default TeacherItem;
