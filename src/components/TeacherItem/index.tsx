b849822af777071379e7a5dc4e76f430616473ed
Matheus Pimentel <matheus08.pimentel@gmail.com>
2020-08-11 21:00:05 -0300

    Create the filters connection with back-end

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -8,29 +8,43 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
         <Image
           style={styles.avatar}
-          source={{ uri: 'https://github.com/matheuscpimentel.png' }}
+          source={{ uri: teacher.avatar }}
         />
 
         <View style={styles.profileInfo}>
-          <Text style={styles.name}>Matheus Pimentel</Text>
-          <Text style={styles.subject}>Programação</Text>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
         </View>
       </View>
 
       <Text style={styles.bio}>
-        Teste testando teste teste teste eeeeeeeeee
+        {teacher.bio}
       </Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>
           Preço/hora {'   '}
-          <Text style={styles.priceValue}>R$20,00</Text>
+          <Text style={styles.priceValue}>R${teacher.cost}</Text>
         </Text>
 
         <View style={styles.buttonsContainer}>
